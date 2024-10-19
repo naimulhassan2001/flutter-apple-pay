66c2555c20f9c4814d03e0a24c61cbbfd2074887
naimulhassan <developernaimul00@gmail.com>
2024-10-19 17:06:38 +0600

    init

---
+++ lib/home_page.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_apple_pay/payment_config.dart';
+import 'package:pay/pay.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String os = Platform.operatingSystem;
+
+  var applePayButton = ApplePayButton(
+    paymentConfiguration: PaymentConfiguration.fromJsonString(defaultApplePay),
+    paymentItems: const [
+      PaymentItem(
+        label: 'Item A',
+        amount: '100',
+        status: PaymentItemStatus.final_price,
+      ),
+      PaymentItem(
+        label: 'Item B',
+        amount: '100',
+        status: PaymentItemStatus.final_price,
+      ),
+      PaymentItem(
+        label: 'Total',
+        amount: '200',
+        status: PaymentItemStatus.final_price,
+      )
+    ],
+    style: ApplePayButtonStyle.black,
+    width: double.infinity,
+    height: 50,
+    type: ApplePayButtonType.buy,
+    onPressed: () => print("sdafd"),
+    margin: const EdgeInsets.only(top: 15.0),
+    onPaymentResult: (result) => debugPrint('Payment Result $result'),
+    loadingIndicator: const Center(
+      child: CircularProgressIndicator(),
+    ),
+  );
+
+  var googlePayButton = GooglePayButton(
+    paymentConfiguration: PaymentConfiguration.fromJsonString(defaultGooglePay),
+    paymentItems: const [
+      PaymentItem(
+        label: 'Total',
+        amount: '0.01',
+        status: PaymentItemStatus.final_price,
+      )
+    ],
+    type: GooglePayButtonType.pay,
+    margin: const EdgeInsets.only(top: 15.0),
+    onPaymentResult: (result) => debugPrint('Payment Result $result'),
+    loadingIndicator: const Center(
+      child: CircularProgressIndicator(),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Center(child: Platform.isIOS ? applePayButton : googlePayButton),
+      ),
+    );
+  }
+}
